6f7936828820dd8f59299b2af2a5acd85e0c9c02
karo19ab <karo19ab@student.cbs.dk>
2019-10-15 09:48:03 +0200

    Log ind

--- Log ind/login.js
+++ Log ind/login.js
@@ -1,10 +1,32 @@
 // Den følgende variable deffinerer antal loginforsøg.
 var attempt = 3;
 // Nedenfor deffineres funktionen "validate()", som bliver brugt ved at trykke på "log ind"-knappen.
+
+var objBrugernavn = [
+    {
+        brugernavn: "Mathias",
+        kodeord: "mathias123"
+    },
+    {
+        brugernavn: "Magnus",
+        kodeord: "magnus123"
+    },
+    {
+        brugernavn: "Alexandra",
+        kodeord: "alexandra123"
+    },
+    {
+        brugernavn: "Kasper",
+        kodeord: "kasper123"
+    }
+]
+
+
 function validate(){
     var brugernavn = document.getElementById("brugernavn").value;
     var kodeord = document.getElementById("kodeord").value;
-    if ( brugernavn == "Formget" && password == "formget#123"){
+
+    /*if ( brugernavn == "objBrugern" && password == "cbsPant123"){
         alert ("Velkommen til CBS Pant");
         // Efter vores velkomstalert vil man blive linket videre til vores forside.
         window.location = "Forside.html";
@@ -22,6 +44,16 @@ function validate(){
             return false;
         }
     }
+     */
+
+    for(i = 0; i < objBrugernavn.length; i++) {
+        if(brugernavn == objBrugernavn[i].brugernavn && kodeord == objBrugernavn[i].kodeord) {
+            alert ("Velkommen til CBS Pant");
+            window.location = "";
+        }
+
+    }
+
 }
 /* Nu vil vi lave funktionen "gemKodeord", der bliver aktiveret, når man trykker på checkboksen.
 Dermed siger vi, at "if" kodeordets type er et 'password', så skal det laves om til typen 'text'. "else" vil kodeordet
