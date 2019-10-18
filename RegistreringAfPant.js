84464aed09ca9056682efec8ee6656424b40c2cf
karo19ab <karo19ab@student.cbs.dk>
2019-10-18 09:31:11 +0200

    Log ind

--- RegistreringAfPant.js
+++ RegistreringAfPant.js
@@ -1,7 +1,19 @@
 this.pantA = document.getElementById('PantA');
+this.pantB = document.getElementById('PantB');
+this.pantC = document.getElementById('PantC');
 
 function udregnPant() {
     localStorage.setItem('pantA', pantA.value);
-    let udregnetPant = pantA.value * 3;
-    alert('Din pant er ' + udregnetPant + ' kr.');
+    let udregnetPantA = pantA.value * 1;
+    let antalPantA = pantA.value;
+    localStorage.setItem('pantB', pantB.value);
+    let antalPantB = pantB.value;
+    let udregnetPantB = pantB.value * 1.5;
+    localStorage.setItem('pantC', pantC.value);
+    let antalPantC = pantC.value;
+    let udregnetPantC = pantC.value * 3;
+
+    alert('Du har indsamlet ' + antalPantA + ' stk. pant A. \nDu har derfor indsamlet ' + udregnetPantA + ' kroners pant A.\n' +
+        '\nDu har indsamlet ' + antalPantB + ' stk. pant B. \nDu har derfor indsamlet ' + udregnetPantB + ' kroners pant B.\n'+
+        '\nDu har indsamlet ' + antalPantC + ' stk. pant C. \nDu har derfor indsamlet ' + udregnetPantC + ' kroners pant C.');
 }
